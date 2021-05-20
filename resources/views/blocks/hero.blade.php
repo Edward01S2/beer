cd0b265d29112fab59fbbc27579f11bcfeb9fae5
edward01s2 <edward.01s2@gmail.com>
2021-05-20 14:55:59 -0600

    Slow animiation

--- resources/views/blocks/hero.blade.php
+++ resources/views/blocks/hero.blade.php
@@ -21,10 +21,10 @@
     </div>
   </div>
   <div class="overflow-hidden bg-c-red-100" x-show="open"
-    x-transition:enter="transition ease-out duration-300"
+    x-transition:enter="transition ease-out duration-400"
     x-transition:enter-start="transform opacity-50 -translate-y-full"
     x-transition:enter-end="opacity-100 transform translate-y-0"
-    x-transition:leave="transition ease-in duration-150"
+    x-transition:leave="transition ease-in duration-300"
     x-transition:leave-start="opacity-100 transform"
     x-transition:leave-end="opacity-50 transform -translate-y-full"
   >
