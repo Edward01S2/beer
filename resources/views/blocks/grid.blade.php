f46e02cababc7da663ccea8bcaed9589e9dfde0f
edward01s2 <edward.01s2@gmail.com>
2021-05-20 13:16:33 -0600

    Fix hover:

--- resources/views/blocks/grid.blade.php
+++ resources/views/blocks/grid.blade.php
@@ -3,7 +3,7 @@
     @if ($items)
       <div class="grid grid-rows-8 sm:grid-cols-2 grid-container lg:grid-cols-4">
         @foreach($items as $item)
-          {{-- @if($item['link']) --}}
+          @if($item['link'])
           <a id="grid-{!! $loop->index !!}" class="border-[#F8F3ED] border-b border-dashed job-box border-opacity-30 p-6 box lg:p-8 group hover:bg-c-red-100 transition duration-300" href="{!! $item['link'] !!}" @if($item['index']) data-index={!! $item['index'] !!} @endif>
             <div class="w-16 h-16 mb-8 md:w-20 md:h-20 md:mb-12 icon-container">
               <div class="relative w-full pb-[100%]">
@@ -20,8 +20,8 @@
               <div class="text-3xl font-bold font-gt text-c-tan-200 md:text-4xl lg:text-3xl xl:text-4xl">{!! $item['stat'] !!}</div>
             </div>
           </a>
-          {{-- @else
-            <div id="grid-{!! $loop->index !!}" class="border-[#F8F3ED] border-b border-dashed box border-opacity-30 p-6 box lg:p-8 group transition duration-300">
+          @else
+            <div id="grid-{!! $loop->index !!}" class="border-[#F8F3ED] border-b border-dashed box border-opacity-30 p-6 box lg:p-8 group transition duration-300 hover:bg-c-red-100">
               <div class="w-16 h-16 mb-8 md:w-20 md:h-20 md:mb-12 icon-container">
                 <div class="relative w-full pb-[100%]">
                   @if($item['icon'])
@@ -37,7 +37,7 @@
                 <div class="text-3xl font-bold font-gt text-c-tan-200 md:text-4xl lg:text-3xl xl:text-4xl">{!! $item['stat'] !!}</div>
               </div>
             </div>
-          @endif --}}
+          @endif
         @endforeach
       </div>
     @endif
